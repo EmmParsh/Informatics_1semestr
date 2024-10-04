91c701cf8a140cfa9b9c29a1bfb9b76f7b26b5d4
EmmParsh <145643823+EmmParsh@users.noreply.github.com>
2024-10-04 13:16:28 +0300

    Update up2.py

--- sem2dz/up2.py
+++ sem2dz/up2.py
@@ -1,8 +1,8 @@
 a = list(input().split(' '))
 b = int(a[0])
 c = a[1]
-for p in range(0, (len(c)//b)+1):
-    d = c[b*p: b*(p+1)]
+for i in range(0, (len(c)//b)+1):
+    d = c[b*i: b*(i+1)]
     k = d[::-1]
     c = c.replace(d, k)
 print(c)
